34e01c781ba8016a4b12ff000e4df079192c85e8
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2025-04-30 14:40:37 +0100

    feat: implement controller and view for index page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,18 @@
 const asyncHandler = require("express-async-handler");
+const db = require("../models/queries");
 
 const getHomePage = asyncHandler(async (req, res) => {
-  res.render("index");
+  const [categoriesList, brandsList, motorcyclesList] = await Promise.all([
+    db.getCategories(),
+    db.getBrands(),
+    db.getMotorcycles(),
+  ]);
+
+  res.render("index", {
+    categoriesList,
+    brandsList,
+    motorcyclesList,
+  });
 });
 
 module.exports = getHomePage;
