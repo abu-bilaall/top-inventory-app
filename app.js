4dab3389e6102916f3be95d2c2e39cd59db42559
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2025-04-30 14:50:28 +0100

    feat: add logic for main app entry point

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("node:path");
+
+// importing routers
+const indexRouter = require("./routes/indexRouter");
+const categoriesRouter = require("./routes/categoriesRouter");
+const brandsRouter = require("./routes/brandsRouter");
+const motorcyclesRouter = require("./routes/motorcyclesRouter");
+
+// initializing app
+const app = express();
+
+// handling incoming form data
+app.use(express.urlencoded({ extended: true }));
+
+// setting up views
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// mounting routers
+app.use("/", indexRouter);
+app.use("/categories", categoriesRouter);
+app.use("/brands", brandsRouter);
+app.use("/motorcycles", motorcyclesRouter);
+
+// the error-buck stops w/ me
+app.use((error, req, res, next) => {
+  console.log(error, req, res, next);
+});
+
+// server listening..
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening at port: ${PORT}`);
+});
